15620cef337d8d3f4903e5f065f7c02383f2e8e2
Varshan-Manish <varshan.manish2022@vitstudent.ac.in>
2025-05-13 00:24:34 +0530

    9th Commit for the day

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { RxGithubLogo } from "react-icons/rx";
 
--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import Reaclt from "react";
 import dynamic from "next/dynamic";
 const HeroContent = dynamic(() => import("../sub/HeroContent"), { ssr: false });
 
