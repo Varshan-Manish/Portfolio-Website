4fc632d56e1d55472865dde538045e20148e1331
Varshan-Manish <varshan.manish2022@vitstudent.ac.in>
2025-05-13 12:15:45 +0530

    15th Commit

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-module.exports = {
-  webpack(config, { isServer }) {
-    if (isServer) {
-      config.externals = ["swiper"];
-    }
-    return config;
-  },
-};
+
+module.exports = nextConfig;
