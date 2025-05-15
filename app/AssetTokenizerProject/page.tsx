38641950ba4494c2be2d79a35e7e8080ce7bdf17
Varshan-Manish <varshan.manish2022@vitstudent.ac.in>
2025-05-15 15:29:03 +0530

    Testing Changes Made

--- app/AssetTokenizerProject/page.tsx
+++ app/AssetTokenizerProject/page.tsx
@@ -11,18 +11,21 @@ const ProjectsPage = () => {
     const updateVideoOffset = () => {
       const width = window.innerWidth;
       let offset;
-      if (width < 768) offset = -260;
-      else if (width >= 768 && width <= 1024) offset = -400;
-      else if (width > 1024 && width <= 1100) offset = -750;
-      else offset = -250;
-
+      if (width < 768) {
+        offset = -260;
+      } else if (width >= 768 && width <= 1024) {
+        offset = -400;
+      } else if (width > 1024 && width <= 1100) {
+        offset = -750;
+      } else {
+        offset = -250;
+      }
       requestAnimationFrame(() => {
         if (videoRef.current) {
           videoRef.current.style.top = `${offset}px`;
         }
       });
     };
-
     updateVideoOffset();
     window.addEventListener("resize", updateVideoOffset);
     window.addEventListener("orientationchange", updateVideoOffset);
